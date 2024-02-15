29f0ea22aedc589e1ffa9cdcc814725c5a179609
shamada <shamada@jeol.co.jp>
2024-02-15 17:11:21 +0900

    MAINT: 検証して Hot Reload できるようにした

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma-service/prisma.service';
 
+import { TestName } from '@repo/db/dist';
+
 @Injectable()
 export class AppService {
   // PrismaServiceをコンストラクタの引数に設定
   constructor(private readonly prisma: PrismaService) {}
 
   getHello(): string {
-    return 'Hello World!';
+    return `Hello ${TestName}!`;
   }
 
   async getUsers() {
--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,1 +1,3 @@
 export * from '@prisma/client'
+
+export const TestName = 'Monorepo'
